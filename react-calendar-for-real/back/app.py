c20fb94691d1ed373fe496cef170073f9bcd21ea
8U2N <tylerburnworthwriting@gmail.com>
2022-05-10 14:11:12 -0700

    Added heroku posgresql

--- react-calendar-for-real/back/app.py
+++ react-calendar-for-real/back/app.py
@@ -2,13 +2,15 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
-import os
+# import os
+
 
 app = Flask(__name__)
 CORS(app)
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fghxflyutyzetp:68ccca52c2d301e5c98bbea9c1681201b47be7cb34a084806f682beac9139576@ec2-3-224-164-189.compute-1.amazonaws.com:5432/d7cpmjpe9h5v33'
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
